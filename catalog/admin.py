e7202a9146af066cd3b7d61b4391579972a6f7fa
Thiago J. Barbalho <thiagojobson@gmail.com>
2019-09-09 07:34:51 -0300

    Customização do admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Author, Genre, Book, BookInstance
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+admin.site.register(Genre)
--- catalog/models.py
+++ catalog/models.py
@@ -25,12 +25,18 @@ class Book(models.Model):
                                help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN',
                             max_length=13,
-                            help_text='13 Character'
+                            help_text='13 Character '
                             '<a href="https://www.isbn-international.org/content/what-isbn">'
                             'ISBN number</a>')
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book.')
+
+    def display_genre(self):
+        """Create a string for the genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
 
     def __str__(self):
         return self.title
