502831e5e11f8f5ac46dafe96a76fb81ca53d44f
Thiago J. Barbalho <thiagojobson@gmail.com>
2019-09-09 17:35:36 -0300

    Adição da home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Book, BookInstance, Author, Genre
+
+
+def index(request):
+    """View function for the home page of the site."""
+    
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authos = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authos,
+    }
+
+    return render(request, 'index.html', context=context)
+
