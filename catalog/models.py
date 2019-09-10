d63f81610af7302886f91ff27892cd0b6b1bf371
thiagojobson <thiagojobson@gmail.com>
2019-09-10 15:39:54 -0300

    Adição das views listar autor e detalhes

--- catalog/models.py
+++ catalog/models.py
@@ -91,6 +91,10 @@ class Author(models.Model):
     class Meta:
         ordering = ['last_name', 'first_name']
 
+    @property
+    def alive(self):
+        return not self.date_of_death
+
     def get_absolute_url(self):
         """Returns the url to access a particular author instance."""
         return reverse('author-detail', args=[str(self.id)])
