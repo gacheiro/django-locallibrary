4f78ef434af9977e86695c8bdb42fa138b28a46d
Thiago J. Barbalho <thiagojobson@gmail.com>
2019-09-11 09:26:21 -0300

    Adição do número de visitas

--- catalog/views.py
+++ catalog/views.py
@@ -12,11 +12,15 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authos = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0) + 1
+    request.session['num_visits'] = num_visits
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authos,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
